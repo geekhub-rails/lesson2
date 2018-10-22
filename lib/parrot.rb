cc5897cfe1f7695dbe7bbd1b01a9b3f3eb54d4ef
luba.t <luba@t.me>
2018-10-22 21:57:03 +0300

    use underscore

--- lib/parrot.rb
+++ lib/parrot.rb
@@ -3,108 +3,108 @@ class Parrot
         @name = name
         @lives = 3
         @mood = "good"
-        @foodInStomach = 4
-        @foodInJeil = 10
-        @cleanWaterCount = 5
-        @inJeil = true
-        @WantsTosleep = false
-        @whantsToFly = true
-        @wantsBanana = false
-        @learnedWords = []
-        p @name + ' was born.'
+        @food_in_stomach = 4
+        @food_in_jeil = 10
+        @clean_water_count = 5
+        @in_jeil = true
+        @wants_to_sleep = false
+        @wants_to_fly = true
+        @wants_banana = false
+        @learned_words = []
+        p  'Parrot ' + @name + ' was born.'
     end
 
-    def displayParrot
+    def display_parrot
         p inspect
     end
 
-    def giveFeed
-        p '#{@name} has eaten the food'
-        @foodInJeil = 10
-        lifeTime
-        @whantsToFly = true
+    def give_food
+        p "#{@name} has eaten the food"
+        @food_in_jeil = 10
+        life_time
+        @wants_to_fly = true
     end
 
-    def giveWater
-        p '#{name} has got the water'
-        @cleanWaterCount = 5
-        lifeTime
+    def give_water
+        p "#{name} has got the water"
+        @clean_water_count = 5
+        life_time
     end
 
-    def openJail
-        @inJeil = false
-        p '#{name} has left the jail'  
+    def open_jail
+        @in_jeil = false
+        p "#{name} has left the jail" 
         @mood = "happy"      
-        lifeTime
-        @whantsToFly = false
+        life_time
+        @wants_to_fly = false
     end
 
-    def returnToJail
-        @inJeil = true
-        p '#{name} return to the jeil'  
+    def return_to_jail
+        @in_jeil = true
+        p "#{name} return to the jeil" 
         @mood = "good"   
-        lifeTime
+        life_time
     end
 
-    def teachNewWord(word)
-        @learnedWords << word
-        @wantsBanana = true
-        lifeTime
+    def teach_new_word(word)
+        @learned_words << word
+        @wants_banana = true
+        life_time
     end
 
-    def turnLightsOff
-        @WantsToSleep = true
-        p '#{name} sleep'
-        lifeTime
+    def turn_lights_off
+        @wants_to_sleep = true
+        p "#{name} sleep"
+        life_time
     end
 
-    def turnLightsOn
-        @WantsToSleep = false
-        p '#{name} wake up'
+    def turn_lights_on
+        @wants_to_sleep = false
+        p "#{name} wake up"
         @mood = "norm"    
-        lifeTime
+        life_time
     end
 
     private
 
     def hungry? 
-        @foodInStomach <= 1      
+        @food_in_stomach <= 1      
     end
 
-    def hasFood?
-        @foodInJeil >= 2
+    def has_food?
+        @food_in_jeil >= 2
     end
     
-    def hasWater?
-        @cleanWaterCount >= 2
+    def has_water?
+        @clean_water_count >= 2
     end
 
     def poops
-        @foodInStomach -= 4 if @foodInStomach >=4
+        @food_in_stomach -= 4 if @food_in_stomach >=4
     end
 
-    def lifeTime
+    def life_time
         poops
         if hungry?
-            if hasFood?
-                @foodInJeil -= 4
-                @foodInStomach += 4
+            if has_food?
+                @food_in_jeil -= 4
+                @food_in_stomach += 4
             else
-                '#{name} screams loudly!'
+                "#{name} screams loudly!"
             end
         end
-        if hasWater?
-            cleanWaterCount -= 2
+        if has_water?
+            @clean_water_count -= 2
         else 
-            '#{name} screams loudly!'
+            "#{name} screams loudly!"
         end             
     end
 
-    def isTalking
-        if @learnedWords.empty?
+    def is_talking?
+        if @learned_words.empty?
             p "#{name} says 'krya-krya'"
         else
-            @learnedWords[rand(@learnedWords.size)] 
+            @learned_words[rand(@learned_words.size)] 
         end            
     end
     
