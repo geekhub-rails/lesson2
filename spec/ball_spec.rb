e1fd40ac75ffd2f4f196eb07d8baa83a7c992a15
luba.t <luba@t.me>
2018-10-25 18:45:58 +0300

    refactoring tests

--- spec/ball_spec.rb
+++ spec/ball_spec.rb
@@ -3,16 +3,27 @@ require_relative '../lib/ball'
 
 RSpec.describe Ball do
   let(:ball){Ball.new}
+  #before { $stdout.stub(:write) }
   subject{ball.shake}
 
   it "returns string" do
-    expect(subject).to be_a(String)
+    is_expected.to be_a(String)
   end
 
-  it "has color" do
-    colors = ["\e[0;91;49m", "\e[0;93;49m", "\e[0;94;49m", "\e[0;92;49m"]
-    has_any_color = colors.any?{| el | subject.include?(el) }
-    expect(has_any_color).to eq(true)
-  end
+  context "colors" do
+    let(:colors){ ["\e[0;91;49m", "\e[0;93;49m", "\e[0;94;49m", "\e[0;92;49m"]}
 
+    it "has color" do
+      has_any_color = colors.any?{| el | subject.include?(el) }
+      expect(has_any_color).to eq(true)
+    end
+
+    context "all_colors" do
+      subject = (1..100).map{Ball.new.shake}
+      it "return_all_colors" do
+        has_every_color = colors.all?{| el | subject.join.include?(el) }
+        expect(has_every_color).to eq(true)
+      end
+    end
+  end
 end
