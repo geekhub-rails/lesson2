ead9ef824b6bccf601df2cea4d0e11ccfda289eb
arutinn <7arutin@gmail.com>
2018-10-28 18:49:29 +0200

    add new ball.rb

--- spec/ball_spec.rb
+++ spec/ball_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Ball do
   let(:answers) { YAML.load_file(File.join(__dir__, '../answers.yml')) }
 
   it { expect(answers).to include(subject.shake) }
-  it { expect(answers).to eql(Ball::ANSWERS) }
+  it { expect(answers).to eql(ANSWERS) }
 
   describe '#shake' do
-    before { stub_const('Ball::ANSWERS', ['ANSWER']) }
+    before { stub_const('ANSWERS', ['ANSWER']) }
 
     it 'prints colorized answer' do
       expect(STDOUT).to receive(:puts).with("\e[31mANSWER\e[0m")
