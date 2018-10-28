052da337c982df956c4633a585bf53551253aec0
vitaliaa <bitali.94@mail.ru>
2018-10-28 19:50:38 +0200

    rbcp

--- Ball.rb
+++ Ball.rb
@@ -6,17 +6,12 @@ class Ball
   def shake
     index = rand(ANSWERS.size)
     case index
-    when 0..4
-      color_string = 31
-    when 5..9
-      color_string = 32
-    when 10..14
-      color_string = 33
-    when 15..19
-      color_string = 34
+    when 0..4 then color_string = 31
+    when 5..9 then color_string = 32
+    when 10..14 then color_string = 33
+    when 15..19 then color_string = 34
     end
     puts "\e[#{color_string}m#{ANSWERS[index]}\e[0m"
     ANSWERS[index]
-    
   end
 end
